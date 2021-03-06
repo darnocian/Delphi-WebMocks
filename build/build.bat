830eea2c73425887d5501eb31e94bd165fb31dc8
Conrad Vermeulen <conrad.vermeulen@gmail.com>
2021-03-06 21:44:46 +0000

    update

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,4 @@
 cd ..\Tests
 msbuild WebMocks.Tests.dproj /t:Build /p:Configuration=%WEBMOCK_CONFIG% /p:platform=%WEBMOCK_PLATFORM%
-C:\apps\BuildTools\Win32\GitHubStatusNotifier.exe -gitsha:%build.vcs.number%% -successif:%ERRORLEVEL% -context:%WEBMOCK_PLATFORM%,%WEBMOCK_CONFIG% -targeturl:https://build-artifacts.sempare.ltd/%GITHUB_OWNER%/%GITHUB_REPO%/%GITSHA%/dunitx-results.xml
+SET STATUS=%ERRORLEVEL%
+C:\apps\BuildTools\Win32\GitHubStatusNotifier.exe -gitsha:%build.vcs.number%% -successif:%STATUS% -context:%WEBMOCK_PLATFORM%,%WEBMOCK_CONFIG% -targeturl:https://build-artifacts.sempare.ltd/%GITHUB_OWNER%/%GITHUB_REPO%/%GITSHA%/dunitx-results.xml
