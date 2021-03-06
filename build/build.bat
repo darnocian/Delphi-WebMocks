c0f8fbf7179b997ca2a9de8e8aca1c16275aa35f
Conrad Vermeulen <conrad.vermeulen@gmail.com>
2021-03-06 21:53:43 +0000

    test

--- build/build.bat
+++ build/build.bat
@@ -1,5 +1,5 @@
 cd ..\Tests
 msbuild WebMocks.Tests.dproj /t:Build /p:Configuration=%WEBMOCK_CONFIG% /p:platform=%WEBMOCK_PLATFORM%
 SET STATUS=%ERRORLEVEL%
-echo C:\apps\BuildTools\Win32\GitHubStatusNotifier.exe -gitsha:%build.vcs.number%% -successif:%STATUS% -context:%WEBMOCK_PLATFORM%,%WEBMOCK_CONFIG% -targeturl:https://build-artifacts.sempare.ltd/%GITHUB_OWNER%/%GITHUB_REPO%/%GITSHA%/dunitx-results.xml
-C:\apps\BuildTools\Win32\GitHubStatusNotifier.exe -gitsha:%build.vcs.number%% -successif:%STATUS% -context:%WEBMOCK_PLATFORM%,%WEBMOCK_CONFIG% -targeturl:https://build-artifacts.sempare.ltd/%GITHUB_OWNER%/%GITHUB_REPO%/%GITSHA%/dunitx-results.xml
+echo C:\apps\BuildTools\Win32\GitHubStatusNotifier.exe -gitsha:%GITSHA% -successif:%STATUS% -context:%WEBMOCK_PLATFORM%,%WEBMOCK_CONFIG% -targeturl:https://build-artifacts.sempare.ltd/%GITHUB_OWNER%/%GITHUB_REPO%/%GITSHA%/dunitx-results.xml
+C:\apps\BuildTools\Win32\GitHubStatusNotifier.exe -gitsha:%GITSHA% -successif:%STATUS% -context:%WEBMOCK_PLATFORM%,%WEBMOCK_CONFIG% -targeturl:https://build-artifacts.sempare.ltd/%GITHUB_OWNER%/%GITHUB_REPO%/%GITSHA%/dunitx-results.xml
